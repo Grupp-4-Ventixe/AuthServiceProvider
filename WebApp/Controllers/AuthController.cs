6019e59eb7f3c3d3db0a983efc77e721cc9fd772
Nino Hägglund <ninomh99@gmail.com>
2025-05-18 15:43:21 +0200

    Moved Auth from AccountSerivceProvider to separate repository
    - Added AuthController with signup and signin methods.
    - Updated .gitignore to include new appsettings files.
    - Removed Controllers folder reference in WebApp.csproj.
    - Eliminated logging configuration from appsettings files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Business.Dtos;
+using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+        private readonly IAuthService _authService = authService;
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpDto formData)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.SignUpAsync(formData);
+            return result.Succeeded
+                ? Ok(result)
+                : Problem(result.Message);
+
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn([FromBody] SignInDto formData)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                var token = await _authService.SignInAsync(formData);
+                return Ok(new { Token = token });
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Unauthorized("Invalid credentials.");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+        }
+    }
+}
